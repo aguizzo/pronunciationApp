14659229621b2eed951b7284175ef42c631ee5d3
aguizzo <andres.guizzo@gmail.com>
2025-02-04 20:42:51 +0100

    feat: UserProfile component and MyProfile page

--- frontend/pronunciationAppFront/src/App.jsx
+++ frontend/pronunciationAppFront/src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from './pages/About.jsx';
-import './styles/App.css';
-import Home from './pages/Home.jsx';
-import Layout from './layout/Layout.jsx';
-import NoPage from './pages/NoPage.jsx';
-import Practice from './pages/Practice.jsx';
-
+import About from "./pages/About.jsx";
+import "./styles/App.css";
+import Home from "./pages/Home.jsx";
+import Layout from "./layout/Layout.jsx";
+import NoPage from "./pages/NoPage.jsx";
+import Practice from "./pages/Practice.jsx";
+import MyProfile from "./pages/MyProfile.jsx";
 
 export default function App() {
-
-
   return (
     <BrowserRouter>
       <Routes>
@@ -17,10 +15,10 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="practice" element={<Practice />} />
           <Route path="about" element={<About />} />
+          <Route path="myprofile" element={<MyProfile />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-      );
-};
-
+  );
+}
