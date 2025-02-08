541a81674fa15c3399d4269039026757378474a2
aguizzo <andres.guizzo@gmail.com>
2025-02-08 17:43:42 +0100

    fix: refactored tests

--- backend/pronunciationAppBack/src/test/java/dev/pronunciationAppBack/appUserTests/AppUserRepositoryTest.java
+++ backend/pronunciationAppBack/src/test/java/dev/pronunciationAppBack/appUserTests/AppUserRepositoryTest.java
@@ -2,6 +2,7 @@ package dev.pronunciationAppBack.appUserTests;
 
 import dev.pronunciationAppBack.model.AppUser;
 import dev.pronunciationAppBack.repository.AppUserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,44 +17,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 public class AppUserRepositoryTest {
 
+    public static final String ID = "1";
+    public static final String EMAIL = "alice@example.com";
+
     @Autowired
     private AppUserRepository appUserRepository;
 
+    private AppUser appUser;
+
+    @BeforeEach
+    public void setup() {
+        appUser = new AppUser(ID, "Alice", EMAIL, "password", 25, LocalDate.now());
+    }
+
     @Test
     public void shouldCreateAppUser() {
-        // given
-        var appUser = new AppUser("1", "Alice", "alice@example.com", "password", 25, LocalDate.now());
+        // given appUser
         // when
         appUserRepository.save(appUser);
         // then
-        Optional<AppUser> createdUser = appUserRepository.findById("1");
+        Optional<AppUser> createdUser = appUserRepository.findById(ID);
         assertThat(createdUser.isPresent()).isTrue();
-        assertThat(createdUser.get().getName()).isEqualTo("Alice");
+        assertThat(createdUser.get().getEmail()).isEqualTo(EMAIL);
     }
 
     @Test
     public void shouldDeleteAppUser() {
         // given
-        var appUser = new AppUser("1", "Alice", "alice@example.com", "password", 25, LocalDate.now());
         appUserRepository.save(appUser);
         // when
-        appUserRepository.deleteById("1");
+        appUserRepository.deleteById(ID);
         // then
-        Optional<AppUser> deletedUser = appUserRepository.findById("1");
+        Optional<AppUser> deletedUser = appUserRepository.findById(ID);
         assertThat(deletedUser).isEmpty();
     }
 
     @Test
     public void shouldUpdateAppUser() {
         // given
-        var appUser = new AppUser("1", "Alice", "alice@example.com", "password", 25, LocalDate.now());
         appUserRepository.save(appUser);
         // when
         String newEmail = "AliceNewEmail@example.com";
         appUser.setEmail(newEmail);
         appUserRepository.save(appUser);
         // then
-        Optional<AppUser> updatedUser = appUserRepository.findById("1");
+        Optional<AppUser> updatedUser = appUserRepository.findById(ID);
         assertThat(updatedUser).isNotEmpty();
         assertThat(updatedUser.get().getEmail()).isEqualTo(newEmail);
     }
@@ -61,12 +69,11 @@ public class AppUserRepositoryTest {
     @Test
     void shouldFindAppUserByEmail() {
         // given
-        var appUser = new AppUser("2", "John", "John@example.com", "password", 25, LocalDate.now());
         appUserRepository.save(appUser);
         // when
-        Optional<AppUser> foundUser = appUserRepository.findByEmail("John@example.com");
+        Optional<AppUser> foundUser = appUserRepository.findByEmail(EMAIL);
         // then
         assertThat(foundUser).isNotEmpty();
-        assertThat(foundUser.get().getEmail()).isEqualTo("John@example.com");
+        assertThat(foundUser.get().getEmail()).isEqualTo(EMAIL);
     }
 }
